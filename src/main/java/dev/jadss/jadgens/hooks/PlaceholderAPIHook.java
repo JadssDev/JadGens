673ec1a546291ff2ea25618a3408ee2e1d86b497
jadss <Jadss.pt.com.pt@gmail.com>
2022-03-27 05:43:23 +0100

    added placeholderapi hook

--- src/main/java/dev/jadss/jadgens/hooks/PlaceholderAPIHook.java
+++ src/main/java/dev/jadss/jadgens/hooks/PlaceholderAPIHook.java
@@ -1,9 +1,15 @@
 package dev.jadss.jadgens.hooks;
 
+import dev.jadss.jadgens.api.MachinesAPI;
+import dev.jadss.jadgens.api.config.interfaces.LoadedMachineConfiguration;
+import dev.jadss.jadgens.api.player.MachinesUser;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * PlaceholderAPI hook.
@@ -67,6 +73,10 @@ public class PlaceholderAPIHook implements Hook {
 
     public static class Expansion extends PlaceholderExpansion {
 
+        private static final String INVALID_PLACEHOLDER = "Invalid action! Please check your manual!";
+        private static final String NOT_ENOUGH_ARGUMENTS = "Not enough arguments! Please check your manual!";
+        private static final String COULD_NOT_FIND_MACHINE = "Could not find machine you were looking for! Please check your manual!";
+
         @Override
         public @NotNull String getIdentifier() {
             return "jadgens";
@@ -83,7 +93,64 @@ public class PlaceholderAPIHook implements Hook {
         }
 
         @Override
-        public String onPlaceholderRequest(final Player player, @NotNull final String params) {
+        public String onPlaceholderRequest(final Player player, @NotNull final String arguments) {
+            String[] params = arguments.split("_");
+
+            if (params.length == 0) {
+                return INVALID_PLACEHOLDER;
+            }
+
+            String targetType = params[0];
+            if (targetType.equalsIgnoreCase("me")) {
+                if (params.length > 1) {
+                    String action = params[1];
+                    if (action.equalsIgnoreCase("total")) {
+                        return "" + MachinesAPI.getInstance().getPlayer(player.getUniqueId()).getMachines().getMachinesInstances().size();
+                    } else if (action.equalsIgnoreCase("limit")) {
+                        return "" + MachinesAPI.getInstance().getPlayer(player.getUniqueId()).getLimitGroup().limit;
+                    } else if (action.equalsIgnoreCase("limitgroup")) {
+                        return MachinesAPI.getInstance().getPlayer(player.getUniqueId()).getLimitGroup().limitGroupName;
+                    } else if (action.equalsIgnoreCase("remaining")) {
+                        int limit = MachinesAPI.getInstance().getPlayer(player.getUniqueId()).getLimitGroup().limit;
+                        if (limit == -1)
+                            return MachinesAPI.getInstance().getGeneralConfiguration().getMessages().infoCommand.infinite;
+                        else
+                            return "" + (limit - MachinesAPI.getInstance().getPlayer(player.getUniqueId()).getMachines().getMachinesInstances().size());
+                    } else if (action.equalsIgnoreCase("type")) {
+                        String machineType = Arrays.stream(params).skip(2L).collect(Collectors.joining("_"));
+                        LoadedMachineConfiguration machineConfiguration = MachinesAPI.getInstance().getMachineConfiguration(machineType);
+                        if (machineConfiguration == null)
+                            return COULD_NOT_FIND_MACHINE;
+
+                        return "" + MachinesAPI.getInstance().getPlayer(player.getUniqueId()).getMachines().getMachines().stream()
+                                .filter(machine -> machine.getMachineConfiguration().equals(machineConfiguration))
+                                .count();
+                    }
+                } else {
+                    return NOT_ENOUGH_ARGUMENTS;
+                }
+            } else if (targetType.equalsIgnoreCase("server")) {
+                if (params.length > 1) {
+                    String action = params[1];
+                    if (action.equalsIgnoreCase("total")) {
+                        return "" + (long) MachinesAPI.getInstance().getMachines().getMachines().size();
+                    } else if (action.equalsIgnoreCase("type")) {
+                        String machineType = Arrays.stream(params).skip(2L).collect(Collectors.joining("_"));
+                        LoadedMachineConfiguration machineConfiguration = MachinesAPI.getInstance().getMachineConfiguration(machineType);
+                        if (machineConfiguration == null)
+                            return COULD_NOT_FIND_MACHINE;
+
+                        return "" + MachinesAPI.getInstance().getMachines().getMachines().stream()
+                                .filter(machine -> machine.getMachineConfiguration().equals(machineConfiguration))
+                                .count();
+                    }
+                } else {
+                    return NOT_ENOUGH_ARGUMENTS;
+                }
+            } else {
+                //Todo: implement a player target placeholder, for now just return Invalid placeholder.
+                return INVALID_PLACEHOLDER;
+            }
             return "N/A";
         }
     }
