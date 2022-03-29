d6ff41c4f5aaceaa82a69c26f1847e24e71d75db
jadss <Jadss.pt.com.pt@gmail.com>
2022-03-29 20:09:34 +0100

    shortened my name

--- src/main/java/dev/jadss/jadgens/commands/sub/VersionSubcommand.java
+++ src/main/java/dev/jadss/jadgens/commands/sub/VersionSubcommand.java
@@ -11,7 +11,7 @@ public class VersionSubcommand {
     public VersionSubcommand(CommandSender bukkitSender) {
         JSender sender = new JSender(bukkitSender);
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                "&3&lJadGens &7>> &eThis &bserver &eis &a&lrunning &3&lJadGens &bv" + JadGens.getInstance().getDescription().getVersion() + " &eby &3Jadss_pt"));
+                "&3&lJadGens &7>> &eThis &bserver &eis &a&lrunning &3&lJadGens &bv" + JadGens.getInstance().getDescription().getVersion() + " &eby &3Jadss"));
         if (JadGens.getInstance().getHookByName("Vault").isAvailable()) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3JadGens &7>> &eHooked into &3Vault&e!"));
         if (JadGens.getInstance().getHookByName("placeholderapi").isAvailable()) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3JadGens &7>> &eHooked into &3PlaceHolderAPI&e!"));
         if (JadGens.getInstance().getHookByName("playerpoints").isAvailable()) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3JadGens &7>> &eHooked into &3PlayerPoints&e!"));
